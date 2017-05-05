403fd9e70799e88f9d50ae853e2e0c0c61288633
mr_maksi <mr_maksi@mail.ru>
2017-05-06 00:34:52 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover({
+        trigger: 'hover',
+        container: 'body',
+        placement: function(context, source) {
+            var position = $(window).width();
+            if ($(window).width() < 991) {
+                return "left";
+            }
+
+            return "top";
+        }
+    });
+});
